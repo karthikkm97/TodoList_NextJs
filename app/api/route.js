bd9247d637b90844b16149f041d7ab931ce9cd5e
karthikkm97 <kathum9739@gmail.com>
2025-03-03 18:23:36 +0530

    first commit

---
+++ app/api/route.js
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { ConnectDB } from '@/lib/config/db';
+import TodoModel from "@/lib/models/TodoModel";
+
+const loadDB = async () => {
+
+    await ConnectDB();
+
+}
+
+loadDB();
+export async function GET(request)
+{
+    const todos = await TodoModel.find({});
+    return NextResponse.json({todos:todos})
+}
+
+export async function POST(request)
+{
+
+    const {title,description}= await request.json();
+    await TodoModel.create({
+        title,
+        description
+    })
+    return NextResponse.json({msg:"Todo Created"})
+}
+
+export async function DELETE(request)
+{
+    const mongoId = request.nextUrl.searchParams.get('mongoId');
+    await TodoModel.findByIdAndDelete(mongoId);
+
+    return NextResponse.json({msg:"Todo Deleted"})
+
+}
+
+export async function PUT(request)
+{
+    const mongoId = request.nextUrl.searchParams.get('mongoId');
+    await TodoModel.findByIdAndUpdate(mongoId,{
+        $set:{
+            isCompleted:true
+        }
+    });
+
+    return NextResponse.json({msg:"Todo Updated"})
+
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex py-3 flex-wrap justify-around'>
+        <h1 className='text-lg font-semibold'>TODO APP</h1>
+        
+        <ul className='flex gap-[40px] text-m'>
+            <li>Home</li>
+            <li>Product</li>
+            <li>About</li>
+            <li>Contact Us</li>
+
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
